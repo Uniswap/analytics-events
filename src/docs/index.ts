885b6d36c9bf16ad59ec4e024d0454b870fbe0c1
dannydaniil <danny.daniil@uniswap.org>
2022-11-09 15:25:41 -0500

    add docs related data

--- src/docs/index.ts
+++ src/docs/index.ts
@@ -6,15 +6,16 @@ export enum DocsPageName {
 }
 
 /**
- * Sections. Disambiguates low-level elements that may share a name.
+ * Sections. Disambiguate low-level elements that may share a name.
  * eg a `back` button in a modal will have the same `element`,
  * but a different `section`.
  */
 export enum DocsSectionName {
-  CURRENCY_INPUT_PANEL = 'swap-currency-input',
-  CURRENCY_OUTPUT_PANEL = 'swap-currency-output',
-  NAVBAR_SEARCH = 'Navbar Search',
-  WIDGET = 'widget',
+  BOTTOM_MENU_LINKS = 'Bottom Menu Links',
+  DEVELOPER_LINKS = 'Developer Links',
+  GETTING_STARTED_LINKS = 'Getting Started Links',
+  QUICK_LINKS = 'Quick Links',
+  WELCOME_LINKS = 'Welcome Links',
 }
 
 /**
@@ -22,24 +23,24 @@ export enum DocsSectionName {
  * Use to identify low-level components given a TraceContext
  */
 export enum DocsElementName {
-  AUTOROUTER_VISUALIZATION_ROW = 'expandable-autorouter-visualization-row',
-  COMMON_BASES_CURRENCY_BUTTON = 'common-bases-currency-button',
-  CONFIRM_SWAP_BUTTON = 'confirm-swap-or-send',
-  CONNECT_WALLET_BUTTON = 'connect-wallet-button',
-  EXPLORE_BANNER = 'explore-banner',
-  EXPLORE_SEARCH_INPUT = 'explore_search_input',
-  IMPORT_TOKEN_BUTTON = 'import-token-button',
-  MAX_TOKEN_AMOUNT_BUTTON = 'max-token-amount-button',
-  NAVBAR_SEARCH_INPUT = 'navbar-search-input',
-  NFT_ACTIVITY_TAB = 'nft-activity-tab',
-  NFT_BUY_BAG_PAY_BUTTON = 'nft-buy-bag-pay-button',
-  NFT_FILTER_BUTTON = 'nft-filter-button',
-  NFT_FILTER_OPTION = 'nft-filter-option',
-  NFT_TRENDING_ROW = 'nft-trending-row',
-  PRICE_UPDATE_ACCEPT_BUTTON = 'price-update-accept-button',
-  SWAP_BUTTON = 'swap-button',
-  SWAP_DETAILS_DROPDOWN = 'swap-details-dropdown',
-  SWAP_TOKENS_REVERSE_ARROW_BUTTON = 'swap-tokens-reverse-arrow-button',
-  TOKEN_SELECTOR_ROW = 'token-selector-row',
-  WALLET_TYPE_OPTION = 'wallet-type-option',
+  BUILD_ORACLE = 'Build Oracle',
+  DISCORD = 'Discord',
+  EMBED_SWAP_WIDGET = 'Embed Swap Widget',
+  FORUM = 'Forum',
+  GITHUB = 'Github',
+  GRANTS = 'Grants',
+  IMPLEMENT_SWAP = 'Implement Swap',
+  PROVIDE_LIQUIDITY = 'Provide Liquidity',
+  SDK = 'SDK',
+  SDK_QUICK_START = 'Sdk Quick Start',
+  SMART_CONTRACTS = 'Smart Contracts',
+  SMART_CONTRACT_OVERVIEW = 'Smart Contract Overview',
+  V3_CONTRACT_ADDRESS = 'V3 Contract Address',
+  V3_CORE = 'V3 Core',
+  V3_PERIPHERY = 'V3 Periphery',
+  V3_SDK = 'V3 SDK',
+  V3_WIDGETS = 'V3 Widgets',
+  WHAT_IS_UNISWAP = 'What is Uniswap',
+  WHITEPAPER = 'WHITEPAPER',
+  WIDGETS = 'WIDGETS',
 }
