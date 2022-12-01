87f7371f01c80df2d47389f829afc29ec468ad34
Adrian Kant <adrian.kant@uniswap.org>
2022-12-01 13:52:18 -0500

    split out by functionality, keep general events

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,9 @@
+// .js imports required for CommonJS
+
 export * from './primitives.js'
 export * from './properties.js'
-export * from './interface/index.js'
+
 export * from './docs/index.js'
+export * from './interface/index.js'
+export * from './nft/index.js'
+export * from './swap/index.js'
---
+++ src/nft/index.ts
@@ -0,0 +1,20 @@
+/**
+ * NFT specific events.
+ */
+export enum NFTEventName {
+    NFT_ACTIVITY_SELECTED = 'NFT Activity Selected',
+    NFT_BUY_ADDED = 'NFT Buy Bag Added',
+    NFT_BUY_BAG_CHANGED = 'NFT Buy Bag Changed',
+    NFT_BUY_BAG_PAY = 'NFT Buy Bag Pay Clicked',
+    NFT_BUY_BAG_REFUNDED = 'NFT Buy Bag Refunded',
+    NFT_BUY_BAG_SIGNED = 'NFT Buy Bag Signed',
+    NFT_BUY_BAG_SUCCEEDED = 'NFT Buy Bag Succeeded',
+    NFT_FILTER_OPENED = 'NFT Collection Filter Opened',
+    NFT_FILTER_SELECTED = 'NFT Filter Selected',
+    NFT_LISTING_COMPLETED = 'NFT Listing Success',
+    NFT_LISTING_SIGNED = 'NFT Listing Signed',
+    NFT_SELL_ITEM_ADDED = 'NFT Sell Item Added',
+    NFT_SELL_SELECTED = 'NFT Sell Selected',
+    NFT_SELL_START_LISTING = 'NFT Sell Start Listing',
+    NFT_TRENDING_ROW_SELECTED = 'Trending Row Selected',
+}
