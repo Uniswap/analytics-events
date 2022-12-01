6efc67f6a8bf80e408e6831701895a3247c054bb
Adrian Kant <adrian.kant@uniswap.org>
2022-12-01 15:03:45 -0500

    lint rule ignore for NFT name

--- src/nft/index.ts
+++ src/nft/index.ts
@@ -23,6 +23,7 @@ export enum NFTEventName {
 /**
  * Known Filter Types for NFTs
  */
+// eslint-disable-next-line @typescript-eslint/naming-convention
  export enum NFTFilterTypes {
     MARKETPLACE = 'Marketplace',
     PRICE_RANGE = 'Price Range',
