25753d5ca4bbddb0a667b363b1d5d942a101228b
Adrian Kant <adrian.kant@uniswap.org>
2022-12-05 11:39:24 -0500

    add script error checking + messages

--- scripts/install-in-repo.sh
+++ scripts/install-in-repo.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+if [[ -z "${ANALYTICS_IMPLEMENTING_REPO_PATH}" ]]; then
+    echo "ANALYTICS_IMPLEMENTING_REPO_PATH not defined in your environment. Check out https://github.com/Uniswap/analytics-events#testing-events for more information."
+    exit -1
+fi
+
+if [[ -z "${ANALYTICS_IMPLEMENTING_REPO_INSTALL}" ]]; then
+    echo "ANALYTICS_IMPLEMENTING_REPO_PATH not defined in your environment. Check out https://github.com/Uniswap/analytics-events#testing-events for more information."
+    exit -1
+fi
+
 mv uniswap-analytics-events-dev.tgz $ANALYTICS_IMPLEMENTING_REPO_PATH/uniswap-analytics-events-dev.tgz
 
 (
