46b26ddfa0895128f4d3e66ea8f823224c9d3b04
Adrian Kant <adrian.kant@uniswap.org>
2022-12-09 14:43:03 -0500

    ci: event definition rules + testing and automation (#14)

---
+++ .github/pull_request_template.md
@@ -0,0 +1,20 @@
+Your PR title must follow [conventional commits](https://www.conventionalcommits.org/en/v1.0.0/#summary) and start with a valid [type](https://github.com/angular/angular/blob/22b96b9/CONTRIBUTING.md#type).
+
+## Background
+
+...
+
+## Changes
+
+- 
+
+## Testing
+
+
+
+#### Before merging, please ensure the following:
+
+- [ ] All events have been approved by both product and engineering
+- [ ] All events have been tested in their consuming package
+- [ ] This PR is titled as `feat` for any new events, or otherwise follows conventional commit standards
+
---
+++ scripts/install-in-repo.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [[ -z "${ANALYTICS_IMPLEMENTING_REPO_PATH}" ]]; then
+    echo "ANALYTICS_IMPLEMENTING_REPO_PATH not defined in your environment. Check out https://github.com/Uniswap/analytics-events#testing-events for more information."
+    exit -1
+fi
+
+if [[ -z "${ANALYTICS_IMPLEMENTING_REPO_INSTALL}" ]]; then
+    echo "ANALYTICS_IMPLEMENTING_REPO_PATH not defined in your environment. Check out https://github.com/Uniswap/analytics-events#testing-events for more information."
+    exit -1
+fi
+
+mv uniswap-analytics-events-dev.tgz $ANALYTICS_IMPLEMENTING_REPO_PATH/uniswap-analytics-events-dev.tgz
+
+(
+    cd $ANALYTICS_IMPLEMENTING_REPO_PATH
+    if [ "$ANALYTICS_IMPLEMENTING_REPO_INSTALL" = "yarn" ]; then
+        CACHE_DIR=$(yarn cache dir)
+        rm -rf $CACHE_DIR/.tmp/*
+        yarn add file:uniswap-analytics-events-dev.tgz
+    elif [ "$ANALYTICS_IMPLEMENTING_REPO_INSTALL" = "npm" ]; then
+        npm install uniswap-analytics-events-dev.tgz
+    else
+        echo "Invalid install type (must be yarn or npm): " + $ANALYTICS_IMPLEMENTING_REPO_INSTALL
+    fi
+)
