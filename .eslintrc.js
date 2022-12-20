eccbcbff349d2841ca31610e1ec51ec79b669de5
Mike <grabbou@gmail.com>
2022-12-21 00:33:48 +0400

    chore: use Uniswap ESLint config

---
+++ .eslintrc.js
@@ -0,0 +1,5 @@
+/* eslint-env node */
+
+module.exports = {
+  extends: '@uniswap/eslint-config/node',
+}
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
+export * from './docs/index.js'
+export * from './interface/index.js'
 export * from './primitives.js'
 export * from './properties.js'
-export * from './interface/index.js'
-export * from './docs/index.js'
