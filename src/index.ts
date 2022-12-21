931e5e588a010710225816c98a8efff6e757740f
Adrian Kant <adrian.kant@uniswap.org>
2022-12-21 11:34:19 -0500

    remove CJS required .js extensions

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
-// .js imports required for CommonJS
+export * from './primitives'
 
-export * from './primitives.js'
-
-export * from './docs/index.js'
-export * from './interface/index.js'
-export * from './nft/index.js'
-export * from './swap/index.js'
+export * from './docs'
+export * from './interface'
+export * from './nft'
+export * from './swap'
