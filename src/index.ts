a1ad323e8e43b0c5e25760d27ae5877c71e75b16
Jordan Frankfurt <jordanwfrankfurt@gmail.com>
2023-01-03 13:33:52 -0600

    feat: FoR events (#29)

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,6 @@ export * from './primitives.js'
 
 export * from './docs/index.js'
 export * from './interface/index.js'
+export * from './moonpay/index.js'
 export * from './nft/index.js'
 export * from './swap/index.js'
--- src/interface/events.ts
+++ src/interface/events.ts
@@ -2,11 +2,14 @@
  * Interface specific events.
  */
 export enum InterfaceEventName {
+  ACCOUNT_DROPDOWN_BUTTON_CLICKED = 'Account Dropdown Button Clicked',
   APPROVE_TOKEN_TXN_SUBMITTED = 'Approve Token Transaction Submitted',
   CONNECT_WALLET_BUTTON_CLICKED = 'Connect Wallet Button Clicked',
   EXPLORE_BANNER_CLICKED = 'Explore Banner Clicked',
   EXPLORE_SEARCH_SELECTED = 'Explore Search Selected',
   EXPLORE_TOKEN_ROW_CLICKED = 'Explore Token Row Clicked',
+  FIAT_ONRAMP_BANNER_CLICKED = 'Fiat Onramp Banner Clicked',
+  FIAT_ONRAMP_WIDGET_OPENED = 'Fiat Onramp Widget Opened',
   NAVBAR_RESULT_SELECTED = 'Navbar Result Selected',
   NAVBAR_SEARCH_EXITED = 'Navbar Search Exited',
   NAVBAR_SEARCH_SELECTED = 'Navbar Search Selected',
