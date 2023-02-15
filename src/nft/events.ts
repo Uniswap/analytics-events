3e93aad64d6db17699af8cc47d10a6fb842af389
Jack Short <john.short.tj@gmail.com>
2023-02-15 13:02:03 -0500

    prefixing with BUY

--- src/nft/events.ts
+++ src/nft/events.ts
@@ -10,6 +10,8 @@ export enum NFTEventName {
   NFT_BUY_BAG_REFUNDED = 'NFT Buy Bag Refunded',
   NFT_BUY_BAG_SIGNED = 'NFT Buy Bag Signed',
   NFT_BUY_BAG_SUCCEEDED = 'NFT Buy Bag Succeeded',
+  NFT_BUY_TOKEN_SELECTED = 'NFT Token Selected',
+  NFT_BUY_TOKEN_SELECTOR_CLICKED = 'NFT Token Selector Clicked',
   NFT_FILTER_OPENED = 'NFT Collection Filter Opened',
   NFT_FILTER_SELECTED = 'NFT Filter Selected',
   NFT_LISTING_COMPLETED = 'NFT Listing Success',
@@ -17,7 +19,5 @@ export enum NFTEventName {
   NFT_PROFILE_PAGE_START_SELL = 'NFT Profile Page Start Sell',
   NFT_SELL_ITEM_ADDED = 'NFT Sell Item Added',
   NFT_SELL_START_LISTING = 'NFT List Page Start Listing',
-  NFT_TOKEN_SELECTED = 'NFT Token Selected',
-  NFT_TOKEN_SELECTOR_CLICKED = 'NFT Token Selector Clicked',
   NFT_TRENDING_ROW_SELECTED = 'Trending Row Selected',
 }
