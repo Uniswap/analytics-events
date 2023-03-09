d84e001aea7e64310b7c2ab686e41391214209ae
Lynn Yu <lynn.yu@uniswap.org>
2023-03-09 13:02:08 -0500

    undo unwanetd

--- src/primitives.ts
+++ src/primitives.ts
@@ -11,27 +11,6 @@ export enum SharedEventName {
   PAGE_VIEWED = 'Page Viewed',
   SEARCH_BAR_CLICKED = 'Search Bar Clicked',
   SENTIMENT_SUBMITTED = 'Sentiment Submitted',
-<<<<<<< HEAD
-  SWAP_AUTOROUTER_VISUALIZATION_EXPANDED = 'Swap Autorouter Visualization Expanded',
-  SWAP_DETAILS_EXPANDED = 'Swap Details Expanded',
-  SWAP_MAX_TOKEN_AMOUNT_SELECTED = 'Swap Max Token Amount Selected',
-  SWAP_MODIFIED_IN_WALLET = 'Swap Modified in Wallet',
-  SWAP_PRICE_UPDATE_ACKNOWLEDGED = 'Swap Price Update Acknowledged',
-  SWAP_QUOTE_RECEIVED = 'Swap Quote Received',
-  SWAP_SIGNED = 'Swap Signed',
-  SWAP_SUBMITTED_BUTTON_CLICKED = 'Swap Submit Button Clicked',
-  SWAP_TOKENS_REVERSED = 'Swap Tokens Reversed',
-  SWAP_TRANSACTION_COMPLETED = 'Swap Transaction Completed',
-  TAX_SERVICE_BANNER_CTA_CLICKED = 'Tax Service Learn More Button Clicked',
-  TAX_SERVICE_COINTRACKER_CLICKED = 'Tax Service CoinTracker Link Clicked',
-  TAX_SERVICE_TOKENTAX_CLICKED = 'Tax Service TokenTax Link Clicked',
-  TOKEN_IMPORTED = 'Token Imported',
-  TOKEN_SELECTED = 'Token Selected',
-  TOKEN_SELECTOR_OPENED = 'Token Selector Opened',
-  WALLET_CONNECT_TXN_COMPLETED = 'Wallet Connect Transaction Completed',
-  WALLET_SELECTED = 'Wallet Selected',
-=======
->>>>>>> main
   WEB_VITALS = 'Web Vitals',
 }
 
