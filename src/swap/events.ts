c9206bc12d47ca6dd06acbeaddd8f8511eba02bd
Eddie Dugan <eddie.dugan@uniswap.org>
2023-05-09 10:44:02 -0700

    feat: add new event name

--- src/swap/events.ts
+++ src/swap/events.ts
@@ -3,6 +3,7 @@
  */
 export enum SwapEventName {
   SWAP_AUTOROUTER_VISUALIZATION_EXPANDED = 'Swap Autorouter Visualization Expanded',
+  SWAP_BUTTON_CLICKED = 'Swap Button Clicked',
   SWAP_DETAILS_EXPANDED = 'Swap Details Expanded',
   SWAP_ERROR = 'Swap Error',
   SWAP_MAX_TOKEN_AMOUNT_SELECTED = 'Swap Max Token Amount Selected',
