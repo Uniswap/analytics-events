1a57f13824bf3294b1b785dc36fb1d17799cd4bb
eddie <66155195+just-toby@users.noreply.github.com>
2023-09-08 14:06:32 -0700

    feat: add new events to migrate from GA

---
+++ src/lp/events.ts
@@ -0,0 +1,10 @@
+/**
+ * LP specific events.
+ */
+export enum LiquidityEventName {
+  ADD_LIQUIDITY = 'Add Liquidity',
+  COLLECT_LIQUIDITY = 'Collect Liquidity',
+  MIGRATE_LIQUIDITY = 'Migrate Liquidity',
+  REMOVE_LIQUIDITY = 'Remove Liquidity',
+  SELECT_FEE_POOL = 'Select Fee Pool',
+}
---
+++ src/lp/index.ts
@@ -0,0 +1,4 @@
+// .js imports required for CommonJS
+
+export * from './events.js'
+export * from './properties.js'
