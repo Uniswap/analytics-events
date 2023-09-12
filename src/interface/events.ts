1c54d3e803a139b0ef1e77d6840daad380b18d5b
eddie <66155195+just-toby@users.noreply.github.com>
2023-09-12 12:21:25 -0700

    feat: add new event name for wallet connected (#56)

--- src/interface/events.ts
+++ src/interface/events.ts
@@ -21,7 +21,7 @@ export enum InterfaceEventName {
   TOKEN_SELECTOR_OPENED = 'Token Selector Opened',
   UNISWAP_WALLET_APP_DOWNLOAD_OPENED = 'Uniswap Wallet App Download Opened',
   UNISWAP_WALLET_MICROSITE_OPENED = 'Uniswap Wallet Microsite Opened',
-  WALLET_CONNECT_TXN_COMPLETED = 'Wallet Connect Transaction Completed',
+  WALLET_CONNECTED = 'Wallet Connected',
   WALLET_PROVIDER_USED = 'Wallet Provider Used',
   WALLET_SELECTED = 'Wallet Selected',
   WRAP_TOKEN_TXN_INVALIDATED = 'Wrap Token Transaction Invalidated',
